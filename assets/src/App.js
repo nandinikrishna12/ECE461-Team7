ebed653ef48a92829af90f734b3448642d614772
nandinikrishna12 <nandinikrishna122@gmail.com>
2023-04-27 02:04:17 -0400

    fixing botched upstream merge

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import CreatePackage from './pages/CreatePackage';
+import UpdatePackage from './pages/UpdatePackage';
+import PackageDetails from './pages/PackageDetails';
+import SearchPackages from './pages/SearchPackages';
+import PackagesList from './pages/PackagesList';
+import RatePackage from './pages/RatePackage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Add a Header and/or Navigation component here */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreatePackage />} />
+          <Route path="/update/:id" element={<UpdatePackage />} />
+          <Route path="/package/:id" element={<PackageDetails />} />
+          <Route path="/search" element={<SearchPackages />} />
+          <Route path="/package/:id/rate" element={<RatePackage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
